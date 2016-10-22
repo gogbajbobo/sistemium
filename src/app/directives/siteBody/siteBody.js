21ea403d328257e352e46b4fe268b6a0ff437218
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-10-22 16:38:39 +0300

    add staff info

--- src/app/directives/siteBody/siteBody.js
+++ src/app/directives/siteBody/siteBody.js
@@ -29,8 +29,58 @@
             title: 'Sistemium',
             currentLang: 'RU',
             langs: ['LT', 'RU', 'EN'],
-            sections: ['About', 'Staff', 'Projects', 'Technologies']
+            sections: ['About', 'Staff', 'Projects', 'Technologies'],
+            staffData: staffData()
         });
+
+        function staffData() {
+
+            return [
+                {
+                    name: 'Mr. John Doe',
+                    position: 'Mr. John Doe — the Boss of the Company',
+                    photoUrl: '',
+                    shortDescription: 'Boss',
+                    descriptionUrl: ''
+                },
+                {
+                    name: 'John Doe Sr.',
+                    position: 'John Doe Sr. — senior developer of the Company',
+                    photoUrl: '',
+                    shortDescription: 'Senior developer',
+                    descriptionUrl: ''
+                },
+                {
+                    name: 'John Doe Jr.',
+                    position: 'John Doe Jr. — junior developer of the Company',
+                    photoUrl: '',
+                    shortDescription: 'Junior developer',
+                    descriptionUrl: ''
+                },
+                {
+                    name: 'John D. Tester',
+                    position: 'John D. Tester — tester of the Company',
+                    photoUrl: '',
+                    shortDescription: 'Tester',
+                    descriptionUrl: ''
+                },
+                {
+                    name: 'Jane Doe',
+                    position: 'Jane Doe — designer of the Company',
+                    photoUrl: '',
+                    shortDescription: 'Designer',
+                    descriptionUrl: ''
+                },
+                {
+                    name: 'Jane Doe Second',
+                    position: 'Jane Doe Second — office manager of the Company',
+                    photoUrl: '',
+                    shortDescription: 'Office manager',
+                    descriptionUrl: ''
+                }
+            ]
+
+        }
 
     }
 
